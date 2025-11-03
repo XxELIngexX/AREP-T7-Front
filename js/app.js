ffbd2db07b1d4a0f72d6e7384b7169f7d14e4ec3
cesar <cesar.amaya.gomes@gmail.com>
2025-11-03 18:33:06 -0500

    return

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 
+const redirectUri = encodeURIComponent("https://xxelingexx.github.io/AREP-T7-Front/profileCreator");
 
-document.getElementById("loginBtn").addEventListener("click", () => {
-  window.location.href =
+window.location.href =
     "https://us-east-1rgr4mlkxk.auth.us-east-1.amazoncognito.com/login" +
     "?client_id=5miflgkc4h2edf4h3408u77c36" +
     "&response_type=code" +
     "&scope=email+openid+profile" +
-    "&redirect_uri=https://xxelingexx.github.io/AREP-T7-Front/profileCreator";
-});
+    "&redirect_uri=" + redirectUri;
+
 
 document.getElementById("signupBtn").addEventListener("click", () => {
   });
